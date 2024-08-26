e56cf2b81701a06e8611c2b9106eb147e0ae805f
PraxisMapper <praxismapper@gmail.com>
2024-08-26 01:23:36 -0400

    Add other water check to component

--- Content.Server/Botany/Systems/PlantGrowthSystem.cs
+++ Content.Server/Botany/Systems/PlantGrowthSystem.cs
@@ -11,6 +11,27 @@ namespace Content.Server.Botany.Systems
 
         //TODO: it is possible that I need a generic call here for OnGrow(), and it implements some setup that
         //pass that along to the right system by the type of the component?
+
+        public void AffectGrowth(int amount, PlantHolderComponent? component = null)
+        {
+            if (component == null || component.Seed == null)
+                return;
+
+            if (amount > 0)
+            {
+                if (component.Age < component.Seed.Maturation)
+                    component.Age += amount;
+                else if (!component.Harvest && component.Seed.Yield <= 0f)
+                    component.LastProduce -= amount;
+            }
+            else
+            {
+                if (component.Age < component.Seed.Maturation)
+                    component.SkipAging++;
+                else if (!component.Harvest && component.Seed.Yield <= 0f)
+                    component.LastProduce += amount;
+            }
+        }
     }
 
     [ByRefEvent]
